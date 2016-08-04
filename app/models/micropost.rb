4d8071bb7882357282a330171f6b9c97ec773f66
Thorin Butler <thorin.butler@gmail.com>
2016-08-04 11:29:03 -0500

    Add ability to validate content field has content

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,6 @@
 class Micropost < ActiveRecord::Base
   belongs_to :user
-  #line below verifies the content field is 140 characters or less
-  validates :content, length: { maximum: 140 }
+  #line below verifies the content exists and is 140 characters or less
+  validates :content, length: { maximum: 140 },
+                      presence: true
 end
